1081603d6a957164124f7bc94389452e6ac08283
Taras Semenchenko <semtaras20@gmail.com>
2022-09-04 15:21:19 +0300

    Move class ESClient to the separate module

---
+++ show_data/src/es_client.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+
+class ESClient:
+    def __init__(self, host_name="elasticsearch", port=9200):
+        self.es = Elasticsearch({"host": host_name, "port": port, "scheme": "http"})
+
+    def get_all_data(self):
+        self.es.indices.refresh(index="main-index")
+        resp = self.es.search(index="main-index", query={"match_all": {}})
+
+        result = []
+        for hit in resp["hits"]["hits"]:
+            record = hit["_source"].copy()
+            record["timestamp"] = str(datetime.strptime(record["timestamp"], "%Y-%m-%dT%H:%M:%S"))
+            result += [record]
+
+        return result
--- show_data/src/main.py
+++ show_data/src/main.py
@@ -1,31 +1,11 @@
 import logging
-from datetime import datetime
 from fastapi import FastAPI
-from elasticsearch import Elasticsearch
+
+from es_client import ESClient
 
 logging.getLogger().setLevel(logging.INFO)
 
 app = FastAPI()
-
-
-class ESClient:
-    def __init__(self, host_name="elasticsearch", port=9200):
-        self.es = Elasticsearch({"host": host_name, "port": port, "scheme": "http"})
-
-    def get_all_data(self):
-        self.es.indices.refresh(index="main-index")
-
-        resp = self.es.search(index="main-index", query={"match_all": {}})
-
-        result = []
-        for hit in resp["hits"]["hits"]:
-            record = hit["_source"].copy()
-            record["timestamp"] = str(datetime.strptime(record["timestamp"], "%Y-%m-%dT%H:%M:%S"))
-            result += [record]
-
-        return result
-
-
 es = ESClient()
 
 
