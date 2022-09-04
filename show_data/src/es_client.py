c6bf5c50f0e06003da937292d2e0661b1c2ba2e2
Taras Semenchenko <semtaras20@gmail.com>
2022-09-04 17:31:20 +0300

    Increase number of returned documents

--- show_data/src/es_client.py
+++ show_data/src/es_client.py
@@ -3,12 +3,13 @@ from elasticsearch import Elasticsearch
 
 
 class ESClient:
-    def __init__(self, host_name="elasticsearch", port=9200):
+    def __init__(self, host_name="elasticsearch", port=9200, size=100):
+        self.size = size
         self.es = Elasticsearch({"host": host_name, "port": port, "scheme": "http"})
 
     def get_all_data(self):
         self.es.indices.refresh(index="main-index")
-        resp = self.es.search(index="main-index", query={"match_all": {}})
+        resp = self.es.search(index="main-index", size=self.size, query={"match_all": {}})
 
         result = []
         for hit in resp["hits"]["hits"]:
